d9561d33d4a284c2a32c4619468b4c924e1e3324
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2023-01-26 14:51:09 +0330

    react was added to laravel with vite

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import './bootstrap';
+import '../css/app.css'
+
+import ReactDOM from 'react-dom/client';        
+import Example from './components/Example';
+
+ReactDOM.createRoot(document.getElementById('app')).render(     
+    <Example />        
+);
